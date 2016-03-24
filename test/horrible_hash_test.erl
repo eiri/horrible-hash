0a411f4bf58347f020758c089cec10d78001ce2b
Eric Avdey <eiri@eiri.ca>
2016-03-23 23:09:08 -0300

    Rename module for edoc's sake

--- test/horrible_hash_test.erl
+++ test/horrible_hash_test.erl
@@ -1,17 +1,17 @@
--module('horrible-hash_test').
+-module(horrible_hash_test).
 
 -include_lib("eunit/include/eunit.hrl").
 
 create_delete_test_() ->
   [
     {"create a hash",
-    ?_assert('horrible-hash':new('$hash'))},
+    ?_assert(horrible_hash:new('$hash'))},
     {"delete the hash",
-    ?_assert('horrible-hash':delete('$hash'))},
+    ?_assert(horrible_hash:delete('$hash'))},
     {"try to delete the deleted hash",
-    ?_assertNot('horrible-hash':delete('$hash'))},
+    ?_assertNot(horrible_hash:delete('$hash'))},
     {"try to delete a non-existing hash",
-    ?_assertNot('horrible-hash':delete('$none'))}
+    ?_assertNot(horrible_hash:delete('$none'))}
   ].
 
 get_set_test_() ->
@@ -21,15 +21,15 @@ get_set_test_() ->
     fun(Name) ->
       {inorder, [
         {"set a value",
-        ?_assert('horrible-hash':set(Name, key, value))},
+        ?_assert(horrible_hash:set(Name, key, value))},
         {"set a value to a none-existing hash",
-        ?_assertNot('horrible-hash':set('$none', key, value))},
+        ?_assertNot(horrible_hash:set('$none', key, value))},
         {"get an existing value",
-        ?_assertEqual(value, 'horrible-hash':get(Name, key))},
+        ?_assertEqual(value, horrible_hash:get(Name, key))},
         {"try to get a non-existing value",
-        ?_assertEqual(undefined, 'horrible-hash':get(Name, ball))},
+        ?_assertEqual(undefined, horrible_hash:get(Name, ball))},
         {"try to get a value from a none-existing hash",
-        ?_assertEqual(undefined, 'horrible-hash':get('$none', key))}
+        ?_assertEqual(undefined, horrible_hash:get('$none', key))}
       ]}
     end
   }.
@@ -38,33 +38,33 @@ exists_delete_test_() ->
   {setup,
     fun() ->
       Name = setup(),
-      'horrible-hash':set(Name, a, 1),
-      'horrible-hash':set(Name, b, 2),
+      horrible_hash:set(Name, a, 1),
+      horrible_hash:set(Name, b, 2),
       Name
     end,
     fun teardown/1,
     fun(Name) ->
       {inorder, [
         {"check an existance of an existing key",
-        ?_assert('horrible-hash':exists(Name, a))},
+        ?_assert(horrible_hash:exists(Name, a))},
         {"check an existance of a non-existing key",
-        ?_assertNot('horrible-hash':exists(Name, z))},
+        ?_assertNot(horrible_hash:exists(Name, z))},
         {"check an existance of a second existing key",
-        ?_assert('horrible-hash':exists(Name, b))},
+        ?_assert(horrible_hash:exists(Name, b))},
         {"delete one of the keys",
-        ?_assert('horrible-hash':delete(Name, a))},
+        ?_assert(horrible_hash:delete(Name, a))},
         {"cornfirm that deleted key is now non-exists",
-        ?_assertNot('horrible-hash':exists(Name, a))},
+        ?_assertNot(horrible_hash:exists(Name, a))},
         {"confirm that the second key still exists",
-        ?_assert('horrible-hash':exists(Name, b))},
+        ?_assert(horrible_hash:exists(Name, b))},
         {"delete the second key",
-        ?_assert('horrible-hash':delete(Name, b))},
+        ?_assert(horrible_hash:delete(Name, b))},
         {"cofirm that deletion is an indepotent operation",
-        ?_assert('horrible-hash':delete(Name, a))},
+        ?_assert(horrible_hash:delete(Name, a))},
         {"check an existance of a key in a none-existing hash",
-        ?_assertNot('horrible-hash':exists('$none', a))},
+        ?_assertNot(horrible_hash:exists('$none', a))},
         {"check a deletion of a key in a none-existing hash",
-        ?_assertNot('horrible-hash':delete('$none', a))}
+        ?_assertNot(horrible_hash:delete('$none', a))}
       ]}
     end
   }.
@@ -76,18 +76,18 @@ keys_test_() ->
     fun({Name, Keys, _}) ->
       {inorder, [
         {"pull keys from a hash",
-        ?_assertEqual(Keys, 'horrible-hash':keys(Name))},
+        ?_assertEqual(Keys, horrible_hash:keys(Name))},
         {"get a proper keys' list after deletion of one of the keys",
         ?_test(begin
-          true = 'horrible-hash':delete(Name, 5),
-          ?assertEqual(tl(Keys), 'horrible-hash':keys(Name))
+          true = horrible_hash:delete(Name, 5),
+          ?assertEqual(tl(Keys), horrible_hash:keys(Name))
         end)},
         {"try to pull keys from a non-existing hash",
-        ?_assertEqual([], 'horrible-hash':keys('$none'))},
+        ?_assertEqual([], horrible_hash:keys('$none'))},
         {"get an empty list from a hash after deletion of all the keys",
         ?_test(begin
-          ['horrible-hash':delete(Name, K) || K <- lists:seq(1, 4)],
-          ?assertEqual([], 'horrible-hash':keys(Name))
+          [horrible_hash:delete(Name, K) || K <- lists:seq(1, 4)],
+          ?assertEqual([], horrible_hash:keys(Name))
         end)}
       ]}
     end
@@ -100,18 +100,18 @@ values_test_() ->
     fun({Name, _, Values}) ->
       {inorder, [
         {"pull list of values from a hash",
-        ?_assertEqual(Values, 'horrible-hash':values(Name))},
+        ?_assertEqual(Values, horrible_hash:values(Name))},
         {"get a proper values' list after deletion of one of the keys",
         ?_test(begin
-          true = 'horrible-hash':delete(Name, 5),
-          ?assertEqual(tl(Values), 'horrible-hash':values(Name))
+          true = horrible_hash:delete(Name, 5),
+          ?assertEqual(tl(Values), horrible_hash:values(Name))
         end)},
         {"try to pull the values from a non-existing hash",
-        ?_assertEqual([], 'horrible-hash':values('$none'))},
+        ?_assertEqual([], horrible_hash:values('$none'))},
         {"get an empty list from a hash after deletion of all the keys",
         ?_test(begin
-          ['horrible-hash':delete(Name, K) || K <- lists:seq(1, 4)],
-          ?assertEqual([], 'horrible-hash':values(Name))
+          [horrible_hash:delete(Name, K) || K <- lists:seq(1, 4)],
+          ?assertEqual([], horrible_hash:values(Name))
         end)}
       ]}
     end
@@ -125,9 +125,9 @@ each_test_() ->
       {inorder, [
         each_generator(Name, 5),
         {"last each call returns empty list",
-        ?_assertEqual([], 'horrible-hash':each(Name))},
+        ?_assertEqual([], horrible_hash:each(Name))},
         {"try to call each on a non-existing hash",
-        ?_assertEqual([], 'horrible-hash':each('$none'))}
+        ?_assertEqual([], horrible_hash:each('$none'))}
       ]}
     end
   }.
@@ -138,13 +138,13 @@ each_test_() ->
 
 setup() ->
   Name = '$hash',
-  true = 'horrible-hash':new(Name),
+  true = horrible_hash:new(Name),
   Name.
 
 setup_filled() ->
   Name = setup(),
   lists:foreach(fun(I) ->
-    'horrible-hash':set(Name, I, I + 100)
+    horrible_hash:set(Name, I, I + 100)
   end, lists:seq(1, 5)),
   Keys = lists:reverse(lists:seq(1, 5)),
   Values = lists:reverse(lists:seq(101, 105)),
@@ -153,7 +153,7 @@ setup_filled() ->
 teardown({Name, _, _}) ->
   teardown(Name);
 teardown(Name) ->
-  true = 'horrible-hash':delete(Name).
+  true = horrible_hash:delete(Name).
 
 each_generator(Name, N) ->
   {generator,
@@ -167,4 +167,4 @@ each_generator(Name, N) ->
 
 make_each_callback(Name, N) ->
   {"each call returns key " ++ integer_to_list(N),
-  ?_assertEqual([{N, N + 100}], 'horrible-hash':each(Name))}.
+  ?_assertEqual([{N, N + 100}], horrible_hash:each(Name))}.
